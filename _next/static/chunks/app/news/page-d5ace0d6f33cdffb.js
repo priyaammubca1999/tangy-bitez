e3eb4f51ae8c05f5981253cfb497d29ec9f33bb7
priyaammubca1999 <foodztangy@gmail.com>
2025-03-18 12:20:31 +0530

    priya

---
+++ _next/static/chunks/app/news/page-d5ace0d6f33cdffb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4834,9160,1327,3928,1007,3457],{4589:function(e,n,s){Promise.resolve().then(s.bind(s,935)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[1015,935,2971,7023,1744],function(){return e(e.s=4589)}),_N_E=e.O()}]);
